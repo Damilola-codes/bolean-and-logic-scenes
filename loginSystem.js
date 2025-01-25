d324c1ba545b328cbf2782e5b0846cb5900c231c
Odeyemi Damilola <odeyemidamilola78@gmail.com>
2025-01-25 19:18:53 +0100

    I updaated this program which gives users three trial upon sgning and if found incorrect with the trial, it'd automatically lock user

--- loginSystem.js
+++ loginSystem.js
@@ -1,14 +1,60 @@
 const username = "Damilola";
 const password = "Naija";
 
-//test case
-let inputUsername = "";
-let inputPassword = "";
 
-//To check if both username and password are correct to pass the system
-if (inputUsername === username && inputPassword === password){
-    alert('Login successful!');
+
+let attempt = 0;
+const maxAttempt = 3;
+
+
+while (attempt < maxAttempt){
+
+    //test case
+    let inputUsername = prompt("Enter your username");
+    let inputPassword = prompt("Enter your password");
+
+    if (inputUsername === username && inputPassword === password){
+        console.log('Login successful!');
+        break;
+    }
+    else{
+        attempt++;
+        console.log(`Invalid credentials. ${attempt} of ${maxAttempt}.`);
+    }
+    ;
+    if (attempt === maxAttempt){
+        console.log("Account Locked!")
+    }
 }
-else{
-    alert('Invalid username or password. Try again.');
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// for (let attempt = 0; attempt <= maxAttempt; attempt++){
+//     if (username && password !== inputUsername && inputPassword){
+//         console.log("Invalid username or password. Try again.")
+//     }
+// }
+
+// //To check if both username and password are correct to pass the system
+
+// else if (inputPassword && inputUsername !== username && password){
+//     console.log("")
+// }
+// else{
+//     console.log('You tried three times. You\'re automatically logged out')
+// }
